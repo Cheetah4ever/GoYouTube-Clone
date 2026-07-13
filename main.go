4da215a903fff33941ec97a318edb1023c9d5b75
Cheetah4ever <guptahbeu@outlook.com>
2026-07-13 17:48:36 +0100

    feat: add HTTP video server

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"flag"
+	"log"
+
 	"goyoutube/server"
 )
 
 func main() {
-	server.Tcp()
+	addr := flag.String("addr", "localhost:8080", "HTTP server address")
+	video := flag.String("video", "client/206294_tiny.mp4", "path to the MP4 video")
+	flag.Parse()
+
+	if err := server.RunHTTP(*addr, *video); err != nil {
+		log.Fatal(err)
+	}
 }
